aef5367837a34fdd51df46966d3fa6150f633443
star909 <shivamsri30@gmail.com>
2020-01-30 11:49:58 +0530

    Reseting user form

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,14 +1,17 @@
+/** Importing angular component and form elements */
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
-
+/**Loading meta data */
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
+/** Class defining the user data to load into the component */
 export class UserProfileComponent implements OnInit {
 userProfileData:Array<any>=[];
+/** User data through formbuilder class */
 userProfileForm = this.fb.group({
             firstName:['',Validators.required],
             lastName:[''],
@@ -20,8 +23,8 @@ userProfileForm = this.fb.group({
             })
 });
 
-  constructor(private fb: FormBuilder) { }
-
+  constructor(private fb: FormBuilder) { }  /**Adding formbuilder class */
+/** Method for submitting form data */
 formSubmit(){
   console.warn(this.userProfileForm.value);
   this.userProfileData.push(this.userProfileForm.value);
